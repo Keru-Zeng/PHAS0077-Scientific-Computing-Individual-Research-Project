2cd2e5e73007ae8816111ad956ea163ed3d0b2d6
Keru Zeng <18042517196@163.com>
2022-07-11 17:33:56 +0100

    command to test functions

---
+++ commands.py
@@ -0,0 +1,5 @@
+from functions import *
+
+nuclei, nps, nrows,ncols=stackloader('1.2%_1Hour_A-Tubulin.lif - Series001.tif', dir_in='tifs - unanalysed/1.2%_1Hour_A-Tubulin/tifs/',plot=True)
+plotpic('1.2%_1Hour_A-Tubulin.lif - Series001.tif', dir_in='tifs - unanalysed/1.2%_1Hour_A-Tubulin/tifs/',plot=True)
+filefolder(dirname='tifs - unanalysed/',plot=True)
