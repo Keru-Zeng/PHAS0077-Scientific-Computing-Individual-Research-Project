bd5bee6198dfeca72e136faedd114fcab9915ebf
Keru Zeng <18042517196@163.com>
2022-07-17 17:04:27 +0100

    new version

--- functions.py
+++ functions.py
@@ -189,35 +189,13 @@ def sep_count_cells(filename=''):
 	# pixel to the nearest zero pixel, then find peaks in this
 	# distance map
     D = ndimage.distance_transform_edt(thresh)
-    localMax = peak_local_max(D, indices=False, min_distance=30, labels=thresh)
+    localMax = peak_local_max(D, indices=False, min_distance=50, labels=thresh)
 	# perform a connected component analysis on the local peaks,
 	# using 8-connectivity, then appy the Watershed algorithm
     markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
     labels = watershed(-D, markers, mask=thresh)
-    print("[INFO] {} unique segments found".format(len(np.unique(labels)) - 1))
+    print("Note: {} unique segments found".format(len(np.unique(labels)) - 1))
     plt.figure(figsize=(10,10))
     plt.imshow(labels,cmap=plt.cm.nipy_spectral)
     plt.axis('off') 
-	# loop over the unique labels returned by the Watershed
-	# algorithm
-    for label in np.unique(labels):
-		# if the label is zero, we are examining the 'background'
-		# so simply ignore it
-        if label == 0:
-            continue
-		# otherwise, allocate memory for the label region and draw
-		# it on the mask
-        mask = np.zeros(gray.shape, dtype="uint8")
-        mask[labels == label] = 255
-		# detect contours in the mask and grab the largest one
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        c = max(cnts, key=cv2.contourArea)
-		# draw a circle enclosing the object
-        ((x, y), r) = cv2.minEnclosingCircle(c)
-        cv2.circle(image, (int(x), int(y)), int(r), (0, 255, 0), 2)
-        cv2.putText(image, "#{}".format(label), (int(x) - 10, int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-	# show the output image
-	#plt.figure(figsize=(10,10))
-	#plt.imshow(image)
-	#plt.axis('off')
+	
